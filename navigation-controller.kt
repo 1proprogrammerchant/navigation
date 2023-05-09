b46cf037d204f363878ddaff9a1504fcc570920f
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-05-08 21:43:50 -0500

    Create navigation-controller.kt

---
+++ navigation-controller.kt
@@ -0,0 +1,36 @@
+val navController = rememberNavController()
+Scaffold(
+  bottomBar = {
+    BottomNavigation {
+      val navBackStackEntry by navController.currentBackStackEntryAsState()
+      val currentDestination = navBackStackEntry?.destination
+      items.forEach { screen ->
+        BottomNavigationItem(
+          icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+          label = { Text(stringResource(screen.resourceId)) },
+          selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+          onClick = {
+            navController.navigate(screen.route) {
+              // Pop up to the start destination of the graph to
+              // avoid building up a large stack of destinations
+              // on the back stack as users select items
+              popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+              }
+              // Avoid multiple copies of the same destination when
+              // reselecting the same item
+              launchSingleTop = true
+              // Restore state when reselecting a previously selected item
+              restoreState = true
+            }
+          }
+        )
+      }
+    }
+  }
+) { innerPadding ->
+  NavHost(navController, startDestination = Screen.Profile.route, Modifier.padding(innerPadding)) {
+    composable(Screen.Profile.route) { Profile(navController) }
+    composable(Screen.FriendsList.route) { FriendsList(navController) }
+  }
+}
