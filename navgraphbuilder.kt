3e542a075d280981c79b1d2408b5e5656c80629c
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-05-10 08:01:06 -0500

    Create navgraphbuilder.kt

---
+++ navgraphbuilder.kt
@@ -0,0 +1,7 @@
+fun NavGraphBuilder.loginGraph(navController: NavController) {
+    navigation(startDestination = "username", route = "login") {
+        composable("username") { ... }
+        composable("password") { ... }
+        composable("registration") { ... }
+    }
+}
